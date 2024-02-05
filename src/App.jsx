07aefa17aa613c6cb0664be1bb8a03051faf34d2
MOnizaBritto <monyza2017@gmail.com>
2024-02-05 19:36:01 -0300

    Desenvolvimento de Contador

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react' // importa react (Não obrigatório)
+import Contador from './contador/contador.jsx'
+
+function App(){
+  return(
+      <>
+        <Contador/>
+      </>
+  )
+}
+
+export default App
